b0b2513e86854fda50f594316a0eb05ecd62104f
trox667 <trox667@googlemail.com>
2021-11-20 14:42:54 +0100

    load input using http

--- api/solutions/day1.py
+++ api/solutions/day1.py
@@ -1,28 +1,33 @@
-import os
+import requests
 
+def load_file():
+    try:
+        response = requests.get('https://aoc-trox667.vercel.app/inputs/input01')
+        if response.status_code == 200:
+            return response.text
+    except Exception:
+        return ''
+    return ''
 
 def part1():
     d = {'(': 1, ')': -1}
-    print('day1', os.getcwd())
-    with open('../../inputs/input01') as file:
-        input_string = file.read()
-        floor = 0
-        for c in input_string:
-            if c in d:
-                floor += d[c]
-        return floor
+    input_string = load_file()
+    floor = 0
+    for c in input_string:
+        if c in d:
+            floor += d[c]
+    return floor
 
     #         print(f'Part 1: {floor}')
 
 
 def part2():
     d = {'(': 1, ')': -1}
-    with open('../../inputs/input01') as file:
-        input_string = file.read()
-        floor = 0
-        for i, c in enumerate(input_string):
-            if c in d:
-                floor += d[c]
-            if floor == -1:
-                #                 print(f'Part 2: {i + 1}')
-                return i + 1
+    input_string = load_file()
+    floor = 0
+    for i, c in enumerate(input_string):
+        if c in d:
+            floor += d[c]
+        if floor == -1:
+            #                 print(f'Part 2: {i + 1}')
+            return i + 1
